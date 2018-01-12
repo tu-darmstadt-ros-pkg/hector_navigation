4bb4e243ca052d8e4a6d3e375364be25fa484aba
Kevin Daun <daun@sim.tu-darmstadt.de>
2018-01-12 11:04:35 +0100

    Removed prints and changed topics

--- hector_timestamp_alignment/src/hector_timestamp_alignment/timestamp_alignment.py
+++ hector_timestamp_alignment/src/hector_timestamp_alignment/timestamp_alignment.py
@@ -8,15 +8,15 @@ from hector_timestamp_alignment.cfg import timestamp_alignmentConfig
 
 class TimestampAlignment:
     def __init__(self, verbose):
-        self.imu_sub = rospy.Subscriber("/imu/data", sensor_msgs.msg.Imu, self.imu_cb)
-        self.imu_aligned_pub = rospy.Publisher("/imu/data_aligned", sensor_msgs.msg.Imu, queue_size=10)
+        self.imu_sub = rospy.Subscriber("/imu/data_original", sensor_msgs.msg.Imu, self.imu_cb)
+        self.imu_aligned_pub = rospy.Publisher("/imu/data", sensor_msgs.msg.Imu, queue_size=10)
         self.srv = Server(timestamp_alignmentConfig, self.dynamic_reconfigure_cb)
-        self.delay = 0.0
+        self.delay = 0.00
 
     def imu_cb(self, imu_msg):
-        print("Before: ", imu_msg.header.stamp)
+        #print("Before: ", imu_msg.header.stamp)
         imu_msg.header.stamp = imu_msg.header.stamp + rospy.Duration(self.delay)
-        print("After: ", imu_msg.header.stamp)
+        #print("After: ", imu_msg.header.stamp)
         self.imu_aligned_pub.publish(imu_msg)
 
     def dynamic_reconfigure_cb(self, config, level):
